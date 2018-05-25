6e3f9b3d004178e73f4d668609e1d1102e6873de
Zebthewizard <burroughszeb@me.com>
2018-05-25 12:14:56 -0500

    Minor Fixes

--- src/js/main.js
+++ src/js/main.js
@@ -85,16 +85,16 @@ function createNeededElements() {
   }
 }
 
-function loadPageIntoDOM(name, fn=function(){}, err=function(){}) {
+function loadPageIntoDOM(name, fn=function(){}) {
   var meta = getPageMetaByName(name)
-  if (!meta) return err(name, '404')
+  if (!meta) return fn(name, '404')
   var div = SniddlCreateEl({
     parent: '#pages',
     attr: {id: 'page-' + name}
   })
-  if(div.classList.contains('page-visible')) return err(name, 'already-visible')
+  if(div.classList.contains('page-visible')) return fn(name, 'already-visible')
   div.classList.add('page-hidden')
-  window.$pages.push(div)
+  window.$pages[name] = div
   fetchPage(meta.getAttribute('content'), res => {
     div.innerHTML = res
     fn(name)
@@ -123,23 +123,38 @@ function getLandingPage() {
   var arr = split.slice(1, split.length)
   window.$page.name = arr[0] || 'index'
   window.$params = arr.slice(1, arr.length)
-  loadPageIntoDOM(window.$page.name, name => showPage(name), (name, err) => {
+  loadPageIntoDOM(window.$page.name, (name, err) => {
     if (err === '404') showPage('404')
     else showPage(name)
   })
 }
 
-readyToLoadPages(function() {
-  window.$pages = []
-  window.$page = {}
-  createNeededElements()
-  getLandingPage()
-  setTimeout(function() {
-    var pages = document.querySelectorAll('meta[property="sniddl:page"]')
-    pages.forEach(page => loadPageIntoDOM(page.name))
-  }, 1000)
-
-  window.addEventListener("hashchange", function () {
+window.reloadPagesFromMeta = function(fn=function(){}) {
+  readyToLoadPages(function() {
+    window.$pages = {}
+    window.$page = {}
+    createNeededElements()
     getLandingPage()
-  });
+    var pages = document.querySelectorAll('meta[property="sniddl:page"]')
+    var found = 0;
+    setTimeout(function() {
+      fn = window.onPagesReloaded || fn
+      pages.forEach(page => loadPageIntoDOM(page.name, (name, err) => {
+        found ++
+        if (found === pages.length) fn()
+      }))
+    }, 500)
+
+    window.addEventListener("hashchange", function () {
+      getLandingPage()
+    });
+  })
+}
+
+reloadPagesFromMeta(() => {
+  console.log('loaded all pages');
 })
+
+window.onPagesReloaded = function() {
+  console.log('asdfasdfas');
+}
